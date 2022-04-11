8c08e282c5b17271fa0b02ac1e1ca9939be6f50c
Horváth Balázs <tcgmilanofficial@gmail.com>
2022-04-11 09:25:26 +0200

    Test:1.0.9

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -4,8 +4,8 @@ export const Work = [
     {
         id:1,
         name:"mbot-mask-detection",
-        description:"Makeblock mBot",
-        tags:["react","gsap","styledComponents"],
+        description:"Makeblock mBot maszk érzékelés",
+        tags:["python","ai","tensorflow","competetion"],
         
         demo:"https://www.youtube.com/watch?v=1TLDsDwSL5g",
         github:"https://github.com/tcgmilan/mbot-mask-detection"
